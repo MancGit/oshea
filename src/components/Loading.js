2de28bddf690dd455c6530299b39adaf0b074da1
Mancgit <mancgit1@gmail.com>
2018-10-19 16:10:43 -0400

    Separated loading styles into css file

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,16 +5,7 @@ const Loading = () => {
   return (
     <div className="text-center mt-5">
       <h2 className="display-4">We're finding you the cheapest fares!</h2>
-      <img
-        src={spinner}
-        alt="Loading..."
-        style={{
-          height: "300px",
-          width: "300px",
-          margin: "auto",
-          display: "block"
-        }}
-      />
+      <img src={spinner} alt="Loading..." />
     </div>
   );
 };
