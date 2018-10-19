d123cf9cc77f047f0e713d375bb5bfe6356fdca4
Mancgit <mancgit1@gmail.com>
2018-10-19 12:17:14 -0400

    Resolved search/polling on API

--- src/actions/departures.js
+++ src/actions/departures.js
@@ -3,10 +3,136 @@ import axios from "axios";
 import moment from "moment";
 
 export const getDepartures = () => async dispatch => {
-  let searchResult = await axios.get("SearchResponse.json");
+  // let searchResult = await axios.get("SearchResponse.json");
 
+  // let pollResult, newLocations, newOperators;
+  // let count = 1;
+
+  // dispatch({
+  //   type: "SET_SEARCH_INITIATED",
+  //   searchInitiated: true
+  // });
+  // dispatch({
+  //   type: "SET_SEARCH_FINALISED",
+  //   searchFinalised: false
+  // });
+
+  // if (!searchResult.data.complete) {
+  //   var interval = setInterval(async function() {
+  //     pollResult = await axios.get(`PollResponse${count}.json`);
+  //     count++;
+
+  //     if (pollResult.data.complete) {
+  //       dispatch({
+  //         type: "SET_SEARCH_INITIATED",
+  //         searchInitiated: false
+  //       });
+  //       dispatch({
+  //         type: "SET_SEARCH_FINALISED",
+  //         searchFinalised: true
+  //       });
+  //       clearInterval(interval);
+  //     }
+  //     //update departures,locations,and providers of search result
+  //     searchResult.data.departures.push(...pollResult.data.departures);
+
+  //     newLocations = pollResult.data.locations.filter(
+  //       location =>
+  //         searchResult.data.locations.filter(o => o.id !== location.id).length >
+  //         0
+  //     );
+
+  //     newOperators = pollResult.data.operators.filter(
+  //       operator =>
+  //         searchResult.data.operators.filter(o => o.id !== operator.id).length >
+  //         0
+  //     );
+
+  //     searchResult.data.operators.push(...newOperators);
+  //     searchResult.data.locations.push(...newLocations);
+
+  //     const departures = searchResult.data.departures.map(departure => ({
+  //       departureTime: moment(Date.parse(departure.departure_time)).valueOf(),
+  //       arrivalTime: moment(Date.parse(departure.arrival_time)).valueOf(),
+  //       duration: departure.duration,
+  //       price: departure.prices.total,
+  //       currency: departure.prices.currency,
+  //       departureCity: searchResult.data.cities.find(
+  //         city => city.id === searchResult.data.origin_city_id
+  //       ).name,
+  //       destinationCity: searchResult.data.cities.find(
+  //         city => city.id === searchResult.data.destination_city_id
+  //       ).name,
+  //       departurePort: searchResult.data.locations.find(
+  //         location => location.id === departure.origin_location_id
+  //       ).name,
+  //       destinationPort: searchResult.data.locations.find(
+  //         location => location.id === departure.destination_location_id
+  //       ).name,
+  //       providerLogo: searchResult.data.operators.find(
+  //         operator => operator.id === departure.operator_id
+  //       ).logo_url
+  //     }));
+
+  //     dispatch({
+  //       type: GET_DEPARTURES,
+  //       payload: departures
+  //     });
+  //   }, 1000);
+  // } else {
+  //   const departures = searchResult.data.departures.map(departure => ({
+  //     departureTime: moment(Date.parse(departure.departure_time)).valueOf(),
+  //     arrivalTime: moment(Date.parse(departure.arrival_time)).valueOf(),
+  //     duration: departure.duration,
+  //     price: departure.prices.total,
+  //     currency: departure.prices.currency,
+  //     departureCity: searchResult.data.cities.find(
+  //       city => city.id === searchResult.data.origin_city_id
+  //     ).name,
+  //     destinationCity: searchResult.data.cities.find(
+  //       city => city.id === searchResult.data.destination_city_id
+  //     ).name,
+  //     departurePort: searchResult.data.locations.find(
+  //       location => location.id === departure.origin_location_id
+  //     ).name,
+  //     destinationPort: searchResult.data.locations.find(
+  //       location => location.id === departure.destination_location_id
+  //     ).name,
+  //     providerLogo: searchResult.data.operators.find(
+  //       operator => operator.id === departure.operator_id
+  //     ).logo_url
+  //   }));
+
+  //   dispatch({
+  //     type: GET_DEPARTURES,
+  //     payload: departures
+  //   });
+
+  //   dispatch({
+  //     type: "SET_SEARCH_INITIATED",
+  //     searchInitiated: false
+  //   });
+  //   dispatch({
+  //     type: "SET_SEARCH_FINALISED",
+  //     searchFinalised: true
+  //   });
+  // }
+
+  let config = {
+    headers: {
+      Accept:
+        "application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/",
+      "X-Busbud-Token": "PARTNER_AHm3M6clSAOoyJg4KyCg7w"
+    }
+  };
+
+  let searchResult = await axios.get(
+    "https://napi.busbud.com/x-departures/f25dvk/dr5reg/2019-08-02?adult=1",
+    config
+  );
+
+  let lastReceivedDepartures = searchResult.data.departures.length;
   let pollResult, newLocations, newOperators;
-  let count = 1;
 
   dispatch({
     type: "SET_SEARCH_INITIATED",
@@ -19,9 +145,10 @@ export const getDepartures = () => async dispatch => {
 
   if (!searchResult.data.complete) {
     var interval = setInterval(async function() {
-      pollResult = await axios.get(`PollResponse${count}.json`);
-      count++;
-
+      pollResult = await axios.get(
+        `https://napi.busbud.com/x-departures/f25dvk/dr5reg/2019-08-02/poll?adult=1&&index=${lastReceivedDepartures}`,
+        config
+      );
       if (pollResult.data.complete) {
         dispatch({
           type: "SET_SEARCH_INITIATED",
@@ -32,8 +159,10 @@ export const getDepartures = () => async dispatch => {
           searchFinalised: true
         });
         clearInterval(interval);
-
       }
+
+      lastReceivedDepartures = pollResult.data.departures.length;
+
       //update departures,locations,and providers of search result
       searchResult.data.departures.push(...pollResult.data.departures);
 
@@ -79,7 +208,7 @@ export const getDepartures = () => async dispatch => {
         type: GET_DEPARTURES,
         payload: departures
       });
-    }, 1000);
+    }, 5000);
   } else {
     const departures = searchResult.data.departures.map(departure => ({
       departureTime: moment(Date.parse(departure.departure_time)).valueOf(),
@@ -118,54 +247,4 @@ export const getDepartures = () => async dispatch => {
       searchFinalised: true
     });
   }
-
-  // let config = {
-  //   headers: {
-  //     Accept:
-  //       "application/vnd.busbud+json; version=2; profile=https://schema.busbud.com/v2/",
-  //     "X-Busbud-Token": "PARTNER_AHm3M6clSAOoyJg4KyCg7w"
-  //   }
-  // };
-
-  // let searchResult = await axios.get(
-  //   "https://napi.busbud.com/x-departures/f25dvk/dr5reg/2019-08-02?adult=1",
-  //   config
-  // );
-
-  // let lastReceivedDepartures = searchResult.data.departures.length;
-  // let pollResult, newLocations, newOperators;
-
-  // if (!searchResult.data.complete) {
-  // var interval = setInterval(async function() {
-  //   pollResult = await axios.get(
-  //     `https://napi.busbud.com/x-departures/f25dvk/dr5reg/2019-08-02/poll?adult=1&&index=${lastReceivedDepartures}`,
-  //     config
-  //   );
-  //   if (pollResult.data.complete) {
-  //     clearInterval(interval);
-  //   }
-
-  //   lastReceivedDepartures = pollResult.data.departures.length;
-
-  //   //update departures,locations,and providers of search result
-  //   searchResult.data.departures.push(...pollResult.data.departures);
-
-  //   newLocations = pollResult.data.locations.filter(
-  //     location =>
-  //       searchResult.data.locations.filter(o => o.id !== location.id).length >
-  //       0
-  //   );
-
-  //   newOperators = pollResult.data.operators.filter(
-  //     operator =>
-  //       searchResult.data.operators.filter(o => o.id !== operator.id).length >
-  //       0
-  //   );
-
-  //   searchResult.data.operators.push(...newOperators);
-  //   searchResult.data.locations.push(...newLocations);
-  // }, 5000);
-  // }
-
-  //}
 };
--- src/selectors/departures.js
+++ src/selectors/departures.js
@@ -5,7 +5,7 @@ const getSortedDepartures = (departures, sortBy) => {
     } else if (sortBy === "latest") {
       return a.departureTime > b.departureTime ? -1 : 1;
     } else if (sortBy === "cheapest") {
-      return a.price > b.price ? -1 : 1;
+      return a.price < b.price ? -1 : 1;
     } else if (sortBy === "fastest") {
       return a.duration < b.duration ? -1 : 1;
     }
